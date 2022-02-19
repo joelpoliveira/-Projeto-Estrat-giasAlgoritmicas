c73ae8b18f68dad8b7a80ad819e63943176fece0
Tomás Mendes <tomasmendes.colegio@gmail.com>
2022-02-19 10:55:13 +0000

    Add Board e Pieces classes

--- TP1/main.py
+++ TP1/main.py
@@ -1,13 +1,48 @@
 from sys import stdin, stdout
 
+
 def read_all():
     return stdin.readlines()
     
 def readln():
     return stdin.readline().rstrip()
     
-def outln():
+def outln(n):
     return stdout.write(str(n) + "\n")
+
+
+class Piece:
+    numbers = []
     
-if __name__ == '__main__':
-    pass
+    def __init__(self, rows, cols):
+        #self.numbers = [[0 for j in range(rows)] for i in range(cols)]
+        self.numbers = [1, 2, 3]
+        
+    def __str__(self):
+        return f"{self.numbers}"
+
+
+class Board:    
+    board = []
+    
+    def __init__(self, rows, cols):
+        self.board = [[Piece(rows, cols) for j in range(rows)] for i in range(cols)]
+    
+    def print_board(self):
+        for i in self.board:
+            print()
+            for j in i:
+                print(j, end='')
+    
+    def solve(self):
+        pass
+
+    def check(self):
+        pass
+
+
+if __name__ == "__main__":
+    Board = Board(2, 2)
+    Board.print_board()
+    #Board.solve()
+    #Board.check()
