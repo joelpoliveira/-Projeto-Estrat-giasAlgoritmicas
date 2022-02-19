b74a9f35e542e07163435410d224b7900344d826
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-02-19 09:57:47 +0000

    .

---
+++ TP1/main.py
@@ -0,0 +1,13 @@
+from sys import stdin, stdout
+
+def read_all():
+    return stdin.readlines()
+    
+def readln():
+    return stdin.readline().rstrip()
+    
+def outln():
+    return stdout.write(str(n) + "\n")
+    
+if __name__ == '__main__':
+    pass
