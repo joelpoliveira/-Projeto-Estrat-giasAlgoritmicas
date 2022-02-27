4f8e747a27143dd1734c663490b6d1a269f74ce5
Tomás Mendes <tomasmendes.colegio@gmail.com>
2022-02-27 10:57:22 +0000

    70 pontos

--- TP1/main.py
+++ TP1/main.py
@@ -1,7 +1,6 @@
 from sys import stdin, stdout, setrecursionlimit
-from collections import deque
 from time import time
-setrecursionlimit(2501)
+setrecursionlimit(2500)
 
 def read_all():
     return stdin.readlines()
@@ -12,9 +11,11 @@ def readln():
 def outln(n = '', end = '\n'):
     return stdout.write(str(n) + end)
 
-class Piece:    
-    def __init__(self, array):
-        self.numbers = [
+index_to_array = {}
+pieces = set()
+
+def create_piece(array, counter):
+    possibilities = [
                             [ [array[0], array[1] ], 
                               [array[3], array[2] ] ], 
                         
@@ -27,162 +28,197 @@ class Piece:
                             [ [array[1], array[2] ],
                               [array[0], array[3] ] ]
                         ]
-        self.now = 0
-        #print("Created: ", self.numbers)
+    piece = [0, possibilities]
+    index_to_array[counter] = piece
+    counter+=1
+    return counter-1
+
+def row(index, n):
+    piece = index_to_array[index]
+    return ' '.join(piece[1][piece[0]][n])
     
-    def row(self, n):
-        return ' '.join(self.numbers[self.now][n])
+def print_row(piece, n):
+    for i in range(2):
+        outln(piece[1][piece[0]][n][i], end = " ")
     
-    def print_row(self, n):
-        for i in range(2):
-            outln(self.numbers[n][i], end = " ")
+def rotate(index, n):
+    return index_to_array[index][1][n]
+
+def match_up(index, piece_numbers):
+    piece = index_to_array[index]
+    piece_now = piece[1][piece[0]]
+    return (piece_now[0][0] == piece_numbers[1][0]) and (piece_now[0][1] == piece_numbers[1][1]) 
+
+def match_left(index, piece_numbers):
+    piece = index_to_array[index]
+    piece_now = piece[1][piece[0]]
+    return (piece_now[0][0] == piece_numbers[0][1]) and (piece_now[1][0] == piece_numbers[1][1]) 
     
-    def rotate(self, n):
-        return self.numbers[n%4]
+def match_bottom(index, piece_numbers):
+    piece = index_to_array[index]
+    piece_now = piece[1][piece[0]]
+    return (piece_numbers[0][0] == piece_now[1][0]) and (piece_numbers[0][1] == piece_now[1][1])
 
-    def match_up(self, piece):
-        return (self.numbers[0][0] == piece.numbers[1][0]) and (self.numbers[0][1] == piece.numbers[1][1]) 
+def match_right(index, piece_numbers):
+    piece = index_to_array[index]
+    piece_now = piece[1][piece[0]]
+    return (piece_numbers[0][0] == piece_now[0][1]) and (piece_numbers[1][0] == piece_now[1][1])
+ 
+def any_match(index, piece_numbers):
+    if match_bottom(index, piece_numbers):
+        return True
+    if match_right(index, piece_numbers):
+        return True
+    if match_up(index, piece_numbers):
+        return True
+    if match_left(index, piece_numbers):
+        return True
+    return False
 
-    def match_left(self, piece):
-        return (self.numbers[0][0] == piece.numbers[0][1]) and (self.numbers[1][0] == piece.numbers[1][1]) 
+def create_board(r,c, first_index):
+    board = [[-1 for i in range(c)]for j in range(r)]
+    board[0][0] = first_index
+    board_all_data = [r,c,1,board]
+    return board_all_data
 
-    def copy(self):
-        return Piece([self.numbers[0][0], self.numbers[0][1], self.numbers[1][1], self.numbers[1][0]])
-    
-    def match_bottom(self, piece_numbers):
-        return (piece_numbers[0][0] == self.numbers[self.now][1][0]) and (piece_numbers[0][1] == self.numbers[self.now][1][1])
+def print_board(board):
+    r = []
+    for i in range(board[0]):
+        for k in range(2):
+            line = ""
+            for j in range(board[1]):
+                line += row(board[3][i][j], k) + "  "
+            r.append( line.rstrip() )
+        r.append("")
+    outln('\n'.join(r), end = "")
 
-    def match_right(self, piece_numbers):
-        return (piece_numbers[0][0] == self.numbers[self.now][0][1]) and (piece_numbers[1][0] == self.numbers[self.now][1][1])
+"""def debug_outln(self):
+    for i in range(self.rows):
+        for j in range(2):
+            for x in range(self.cols):
+                if self.board[i][x] is not None:
+                    self.board[i][x].print_row(j)
+                else:
+                    print("|_|", end = '')
+                outln('', end = ' ')
+            outln('\n') if j == self.rows - 1 and i != self.rows - 1 else outln()
+"""
+def get_next(board):
+    row = board[2]//board[1]
+    col = board[2]%board[1]
 
-class Board:   
+    return row,col
 
-    def __init__(self, rows, cols, first_piece):
-        self.board = [[None for i in range(cols)] for j in range(rows)]
-        self.board[0][0] = first_piece
-        self.rows = rows
-        self.cols = cols
-        self.next_empty = 1
-     
-    def __str__(self):
-        r = []
-        for i in range(self.rows):
-            for k in range(2):
-                line = ""
-                for j in range(self.cols):
-                    line += self.board[i][j].row(k) + "  "
-                r.append( line.rstrip() )
-            r.append("")
-        return '\n'.join(r)
+def get_current(board):
+    row = (board[2] - 1)//board[1]
+    col = (board[2] - 1)%board[1]
 
-    def debug_outln(self):
-        for i in range(self.rows):
-            for j in range(2):
-                for x in range(self.cols):
-                    if self.board[i][x] is not None:
-                        self.board[i][x].print_row(j)
-                    else:
-                        print("|_|", end = '')
-                    outln('', end = ' ')
-                outln('\n') if j == self.rows - 1 and i != self.rows - 1 else outln()
+    return row,col
 
-    def get_next(self):
-        row = self.next_empty//self.cols
-        col = self.next_empty%self.cols
-        return row,col
+def insert(board, index):
+    r,c = get_next(board)
+    board[3][r][c] = index
+    board[2] += 1
 
-    def get_last(self):
-        row = (self.next_empty - 1)//self.cols
-        col = (self.next_empty - 1)%self.cols
-        return row,col
+def piece_fits(board, piece_numbers):
+    r,c = get_next(board)
 
-    def insert(self, Piece):
-        r,c = self.get_next()
-        self.board[r][c] = Piece
-        self.next_empty+=1
+    if r == 0:
+        if match_right(board[3][r][c - 1], piece_numbers):
+            return True
+        return False
 
-    def piece_fits(self, piece_numbers):
-        r,c = self.get_next()
-        #print(f"{r}--{c}")
-        if r == 0:
-            if self.board[r][c-1].match_right(piece_numbers):
-                return True
-            return False
+    if c == 0:
+        if match_bottom(board[3][r - 1][c], piece_numbers):
+            return True
+        return False
 
-        elif c == 0:
-            if self.board[r - 1][c].match_bottom(piece_numbers):
-                return True
-            return False
-
-        else:
-            if self.board[r - 1][c].match_bottom(piece_numbers) and self.board[r][c - 1].match_right(piece_numbers):
-                return True
-            return False
-
-    def swap(self):
-        pass
-    
-    def solve(self):
-        pass
-    
-    def is_complete(self):
-       return (self.next_empty)==(self.rows*self.cols)
+    if match_bottom(board[3][r - 1][c], piece_numbers) and match_right(board[3][r][c-1], piece_numbers):
+        return True
+    return False
+   
+def is_complete(board):
+    return (board[2])==(board[0]*board[1])
 	
-    def pop(self):
-        r,c = self.get_last()
-        piece = self.board[r][c]
-        self.board[r][c] = None
-        self.next_empty-=1
-        return piece
+def pop(board):
+    r,c = get_current(board)
+    index = board[3][r][c]
+    board[3][r][c] = -1
+    board[2] -= 1
+    return index
 
-    def copy(self):
-        new_b = Board(self.rows, self.cols, self.board[0][0])
-        new_b.board = self.board.copy()
-        return new_b
+def get_current_piece(board):
+    r,c = get_current(board)
+    return board[3][r][c]
 
+def get_current_to_search(board):
+    r,c = get_current(board)
+    if c==board[1]-1:
+        return board[3][r][0]
+    return board[3][r][c]
+
+def resolve(board, subsets):
+    #board.debug_outln()
     
-def resolve(board, pieces_to_use):
-    counter = 0
-    while True:
-        #bool(empty list) = False
-        if counter < len(pieces_to_use):
-            #try all piece rotations
-            for _ in range(4):
-                if ( board.piece_fits(pieces_to_use[0].rotate(_)) ):
-                    pieces_to_use[0].now = _
-                    board.insert( pieces_to_use.popleft())
+    if is_complete(board):
+        return True
 
-                    #piece fits, try next
-                    resolved = resolve(board, pieces_to_use)
+    current = get_current_to_search(board)
+    
+    for i in subsets[current]:
+        if i in pieces:
+            for rot in range(4):
+                if piece_fits(board, rotate(i, rot) ):
+                    index_to_array[i][0] = rot
+                    insert(board, i )
+                    pieces.remove( i )
 
-                    if resolved:
+                    result = resolve(board, subsets)
+                    if result:
                         return True
-                    else:
-                        pieces_to_use.appendleft( board.pop() )
+                    
+                    pieces.add( pop(board) )
 
-            pieces_to_use.append( pieces_to_use.popleft() )
-            counter+=1
-        else:			
-            if board.is_complete():
-                return True
-            return False
+    return False
 
 if __name__ == "__main__":
     n = int(readln())
 
-    # Number of pieces, Rows, Cols
     for _ in range(n):
         N, R, C = list(map(int, readln().split()))
 
-        first_piece = Piece(readln().split())
+        first_index = create_piece(readln().split(), 0)
+
         # Create Board
-        board = Board(R, C, first_piece)
+        board = create_board(R, C, first_index)
 
-        pieces_to_use = deque( Piece( readln().split() ) for i in range(N - 1) )
+        pieces_subset = {first_index : []}
 
-        start = time()
-        if resolve(board, pieces_to_use):
-            outln(board, end = "")
+        for __ in range(N-1):
+            index = create_piece( readln().split(), __ + 1 ) 
+            pieces.add(index)
+            pieces_subset[index] = []
+
+            for i in pieces_subset:
+                if i!=index:
+                    for rot in range(4):
+
+                        if i!=first_index:
+                            if any_match(i, rotate(index, rot)):
+                                pieces_subset[i].append(index)
+                                pieces_subset[index].append(i)
+                                break
+
+                        else:
+                            if match_right(i, rotate(index, rot) ) or match_bottom( i, rotate(index, rot) ):
+                                pieces_subset[i].append(index)
+                                break
+        #print(index_to_array)
+        #start = time()
+        if resolve(board, pieces_subset):
+            #outln(time()-start)
+            print_board(board)
         else:
+            #outln(time()-start)
             outln("impossible puzzle!")
-        outln(time()-start)
+        
