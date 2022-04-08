768bb9f60d06346c211489777e76b271273aca05
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2022-04-08 19:38:08 +0100

    cenas para TP3

---
+++ TP3/main.cpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <chrono>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+
+  return 0;
+}
