66a4c3b22d86670691aa8123d040f8a0794f5388
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2022-04-15 18:00:03 +0100

    Add indgree. Minor changes

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -22,24 +22,25 @@ void print_graph() {
   for (int x = 0; x < n; x++) {
     std::cout << x + 1 << " -> ";
     for (int y = 0; y < graph[x * 1000]; y++) {
-      std::cout << graph[x * 1000 + 1 + y] + 1 << " ";
+      std::cout << graph[x * 1000 + 2 + y] + 1 << " ";
     }
     std::cout << "\n";
   }
   std::cout << "\n";
 }
 
+
 bool dfs(int node) {
   int child;
   marks[node] = 1;
 
   for (int i = 0; i < graph[node * 1000]; i++) {
-    child = graph[node * 1000 + 1 + i];
+    child = graph[node * 1000 + 2 + i];
     std::cout << "node: " << node + 1 << " child = " << child + 1 << "\n";
 
-    // se vai a nó que já foi visitado, tem um ciclo
+    // se vai a nó que já foi visitado, pode ter um ciclo
     if (marks[child] == 1) {
-      // std::cout << "Loop\n";
+      std::cout << "Loop\n";
       return 0;
     } else
       dfs(child);
@@ -54,7 +55,7 @@ void topological_dfs(int node) {
   marks[node] = 1;
 
   for (int i = 0; i < graph[node * 1000]; i++) {
-    child = graph[node * 1000 + 1 + i];
+    child = graph[node * 1000 + 2 + i];
     if (marks[child] == 0)
       topological_dfs(child);
   }
@@ -65,17 +66,15 @@ void topological_dfs_2(int node) {
   int child, temp = graph[node * 1000], best = 0;
   marks[node] = 1;
 
-  // Percorrer os filhos de node 
+  // Percorrer os filhos de node
   for (int i = 0; i < temp; i++) {
-    child = graph[node * 1000 + 1 + i];
+    child = graph[node * 1000 + 2 + i];
 
     // If multiple operations can be done at once, sum which one takes the
     // longest
     if (temp > 1) {
       for (int j = 0; j < temp; j++) {
-
       }
-  
     }
 
     if (marks[child] == 0) {
@@ -90,7 +89,7 @@ bool connectivity() {
   memset(marks, 0, sizeof(int) * n);
 
   // se contem ciclo, é invalido
-  if (dfs(start_node) == 0)
+  if (dfs(start_node) == 0) // TODO usar topology em vez de dfs 
     return 0;
 
   std::cout << "did DFS\n";
@@ -131,10 +130,6 @@ void single_operation() {
 
   topological_dfs(start_node);
 
-  // for (int i = 0; i < n; i++){
-  //   total_time += operation_time[i];
-  // }
-
   std::cout << total_time << '\n';
 
   while (!q.empty()) {
@@ -164,6 +159,12 @@ int main() {
   memset(operation_time, 0, sizeof(int) * 1000);
   memset(graph, 0, sizeof(int) * 101000);
 
+  /*
+    graph[i * 1000] -> outdegree (number of children)
+    graph[i * 1000 + 1] -> indegree
+    graph[i * 1000 + 2 + n] -> children of node i, n => [0, 9]
+  */
+
   int m, i, j, op, aux;
   bool start_declared = 0;
 
@@ -173,6 +174,9 @@ int main() {
 
     // calculate total_time as input is read
     total_time += operation_time[i];
+
+    // calculate indgree
+    graph[i * 1000 + 1] = m;
 
     if (m == 0) { // check for multiple start nodes
       if (start_declared) {
@@ -185,14 +189,16 @@ int main() {
         start_declared = 1;
       }
     } else {
+      // sort out connections 
       for (j = 0; j < m; j++) {
         std::cin >> op;
-        op--; // convert range [1, N] to [0, N-1]
+        op--; // convert range from [1, N] to [0, N-1]
 
         // increment outdegree
         aux = graph[op * 1000]++;
+
         // insert into adjacency list
-        graph[op * 1000 + 1 + aux] = i;
+        graph[op * 1000 + 2 + aux] = i;
 
         // std::cout << "Inserted: " << op + 1 << " - " << i + 1 << "\n";
         // print_graph();
@@ -200,8 +206,13 @@ int main() {
     }
   }
 
-  // Print list for debug
+  // Print list for debuging
   print_graph();
+
+  // Print indgree for debugging
+  // for (i = 0; i < n; i++) {
+  //   std::cout << i + 1 << " - " << graph[i * 1000 + 1] << "\n";
+  // }
 
   if (!check_valid()) {
     std::cout << "INVALID\n";
@@ -219,11 +230,11 @@ int main() {
     break;
   case 2:
     multi_operation();
-    //std::cout << "Operation 2\n";
+    // std::cout << "Operation 2\n";
     break;
   case 3:
     find_bottleneck();
-    //std::cout << "Operation 3\n";
+    // std::cout << "Operation 3\n";
     break;
   }
 
