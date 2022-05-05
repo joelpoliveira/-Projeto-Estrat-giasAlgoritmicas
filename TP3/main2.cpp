c9dc567af2644630ea93418d0de363bffb71c16d
Joel Oliveira <58565967+joelpoliveira@users.noreply.github.com>
2022-05-05 21:39:18 +0100

    120 pontos

--- TP3/main2.cpp
+++ TP3/main2.cpp
@@ -15,53 +15,48 @@
 
 using namespace std;
 
-int *operation_time, 
-    *graph, 
-    *marks, 
-    *recursion_stack,
-    *dp,
-    start_node, 
-    end_node, 
-    n,
-    best, 
-    total_time = 0;
+#define COL_SIZE 1002
 
-deque<int> q;
+int *operation_time, *graph, *marks, *recursion_stack, *dp, start_node,
+    end_node, n, best, total_time;
 
+deque<int>  q, 
+            q1, 
+            q2, 
+            bottleneck;
 
 int dfs(int node) {
   int child;
-
+cout<<"node = "<<node<<"\n";
   marks[node] = 1;
   recursion_stack[node] = 1;
 
-  for (int i = 0; i < graph[node * 1000]; i++) {
-    child = graph[node * 1000 + 2 + i];
-    //std::cout << "node: " << node + 1 << " child = " << child + 1 << "\n";
+  for (int i = 0; i < graph[node * COL_SIZE]; i++) {
+    child = graph[node * COL_SIZE + 2 + i];
+    // std::cout << "node: " << node + 1 << " child = " << child + 1 << "\n";
 
     if (marks[child] == 0) {
-        if (dfs(child) == 0)
-            return 0;
-        if (dp[node]<operation_time[node] + dp[child])
-            dp[node] = operation_time[node] + dp[child];
-    } else{
-        if (recursion_stack[child] == 1){ 
-            return 0;
-        }
-        if (dp[node]<operation_time[node] + dp[child])
-            dp[node] = operation_time[node] + dp[child];
+      if (dfs(child) == 0)
+        return 0;
+      if (dp[node] < operation_time[node] + dp[child])
+        dp[node] = operation_time[node] + dp[child];
+    } else {
+      if (recursion_stack[child] == 1) {
+        return 0;
+      }
+      if (dp[node] < operation_time[node] + dp[child])
+        dp[node] = operation_time[node] + dp[child];
     }
   }
 
-    if(dp[node]==-1)
-        dp[node]=operation_time[node];
-    recursion_stack[node] = 0; // remove node from stack
-    return 1;
+  if (dp[node] == -1)
+    dp[node] = operation_time[node];
+  recursion_stack[node] = 0; // remove node from stack
+  return 1;
 }
 
-
 int connectivity() {
-  //std::cout << "n = " << n << " start_node = " << start_node + 1 << "\n";
+  // std::cout << "n = " << n << " start_node = " << start_node + 1 << "\n";
 
   memset(marks, 0, sizeof(int) * n);
 
@@ -70,7 +65,7 @@ int connectivity() {
 
   for (int i = 0; i < n; i++) {
     if (marks[i] == 0) {
-      std::cout << "node " << i + 1 << " not marked\n";
+      // std::cout << "node " << i + 1 << " not marked\n";
       return 0;
     }
   }
@@ -78,13 +73,12 @@ int connectivity() {
   return 1;
 }
 
-
 bool check_valid() {
   int null_out_count = 0;
 
   for (int i = 0; i < n; i++) {
     // if node_i out-degree is 0
-    if (graph[i * 1000] == 0) {
+    if (graph[i * COL_SIZE] == 0) {
       end_node = i;
       if (++null_out_count != 1)
         return 0;
@@ -98,19 +92,17 @@ bool check_valid() {
   return connectivity();
 }
 
-
 void topological_dfs(int node) {
   int child;
   marks[node] = 1;
 
-  for (int i = 0; i < graph[node * 1000]; i++) {
-    child = graph[node * 1000 + 2 + i];
+  for (int i = 0; i < graph[node * COL_SIZE]; i++) {
+    child = graph[node * COL_SIZE + 2 + i];
     if (marks[child] == 0)
       topological_dfs(child);
   }
   q.push_front(node + 1);
 }
-
 
 void single_operation() {
   memset(marks, 0, sizeof(int) * n);
@@ -126,14 +118,146 @@ void single_operation() {
 }
 
 
-void multi_operation() {}
+int sum(){
+    int s = 0;
+    for (int i = 0; i <1000; i++){
+        s+=marks[i];
+    }
+    return s;
+}
 
 
-void find_bottleneck() {}
+void find_bottleneck() {
+    int is_q1=1, 
+        current_node, 
+        current_node_index,
+        in_queue,
+        next_proc,
+        next_proc_count,
+        child, 
+        i;
+    
+    bottleneck.push_back(start_node+1);
+    q1.push_back(start_node);
+    in_queue = 1;
+    next_proc_count = 0;
+    
+    while (!q1.empty() || !q2.empty()){
+        //cout<<is_q1<<"\n";
+        if (is_q1){
+            if (!q1.empty()){
+                current_node = q1.front();
+                q1.pop_front();
 
+                current_node_index = current_node * COL_SIZE;
+
+                next_proc = 0;
+                for (i = 0; i < graph[current_node_index]; i++){
+                    child = graph[current_node_index + 2 + i];
+
+                    if (--graph[child * COL_SIZE + 1]==0){
+                        next_proc = 1;
+                        q2.push_back(child);
+                    }
+                }
+                next_proc_count += next_proc;
+
+                if (q1.empty()){
+                    is_q1=0;
+                    
+                    cout<<"inc = "<<next_proc_count<<"\tprev_len = "<<in_queue<<"\tnode ="<<q2.front()<<"\n";
+                    
+                    if(q2.size() == 1 && next_proc_count == in_queue){
+                        bottleneck.push_back(q2.front() + 1);
+                    }
+                    in_queue = q2.size();
+                    next_proc_count = 0;
+                }
+            }
+        }else{
+            if (!q2.empty()){
+                current_node = q2.front();
+                q2.pop_front();
+                
+                current_node_index = current_node * COL_SIZE;
+                
+                next_proc=0;
+                for (i = 0; i < graph[current_node_index]; i++){
+                    child = graph[current_node_index + 2 + i];
+                    
+                    if (--graph[child * COL_SIZE + 1]==0){
+                        next_proc=1;
+                        q1.push_back(child);
+                    }
+                }
+                next_proc_count+=next_proc;
+    
+                if (q2.empty()){
+                    is_q1=1;
+                    
+                    cout<<"inc = "<<next_proc_count<<"\tprev_len = "<<in_queue<<"\tnode ="<<q1.front()<<"\n";
+                    
+                    if(q1.size() == 1 && next_proc_count == in_queue){
+                        bottleneck.push_back(q1.front() + 1);
+                    }
+                    in_queue = q1.size();
+                    next_proc_count = 0;
+                }
+            }            
+        }
+    }
+    
+    
+    while( !bottleneck.empty() ){
+        cout<<bottleneck.front()<<"\n";
+        bottleneck.pop_front();
+    }
+}
+
+
+/*
+void top_dsf(int node, int prev){
+    
+    marks[node] = 1;
+    
+    if (--graph[node * COL_SIZE + 1] == 0){
+        cout<<"node = "<<node<<"\n";
+        
+        if (sum()==1){
+            bottleneck.push_back(node+1);
+        }
+        marks[prev] = 0;
+        
+        int child;
+        for (int i = 0; i <graph[node * COL_SIZE]; i++){
+            child = graph[node*COL_SIZE + 2 +i];
+            top_dsf(child, node);
+        }   
+    }else{
+        marks[prev] = 0;
+    }
+}
+
+void find_bottleneck(){
+    memset(marks, 0, sizeof(int) * 1000);
+    marks[start_node] = 1;
+    bottleneck.push_back(start_node+1);
+
+    int child;
+    for (int i = 0; i < graph[start_node * COL_SIZE]; i++){
+        child = graph[start_node * COL_SIZE + 2 + i];
+        top_dsf(child, start_node);
+    }
+
+    while( !bottleneck.empty() ){
+        cout<<bottleneck.front()<<"\n";
+        bottleneck.pop_front();
+    }
+}
+*/
 
 int main() {
-    std::ios_base::sync_with_stdio(0);
+  std::ios_base::sync_with_stdio(0);
   std::cin.tie(0);
 
   operation_time = new int[1000];
@@ -149,16 +273,16 @@ int main() {
   memset(marks, 0, sizeof(int) * 1000);
 
   int m, i, j, op, aux;
-  bool start_declared = 0;
+  int start_declared = 0;
 
   std::cin >> n;
   for (i = 0; i < n; i++) {
     std::cin >> operation_time[i] >> m;
 
     total_time += operation_time[i];
-    graph[i * 1000 + 1] = m;
+    graph[i * COL_SIZE + 1] = m;
 
-    if (m == 0) { 
+    if (m == 0) {
       if (start_declared) {
         std::cout << "INVALID\n";
         return 0;
@@ -170,10 +294,9 @@ int main() {
     } else {
       for (j = 0; j < m; j++) {
         std::cin >> op;
-        op--; 
-        aux = graph[op * 1000]++;
-        graph[op * 1000 + 2 + aux] = i;
-
+        op--;
+        aux = graph[op * COL_SIZE]++;
+        graph[op * COL_SIZE + 2 + aux] = i;
       }
     }
   }
@@ -192,16 +315,16 @@ int main() {
     single_operation();
     break;
   case 2:
-    cout<<dp[start_node]<<"\n";
+    cout << dp[start_node] << "\n";
     break;
   case 3:
     find_bottleneck();
     break;
   }
 
-  delete [] operation_time;
-  delete [] graph;
-  delete [] marks;
-  delete [] dp;
-  delete [] recursion_stack;
+  delete[] operation_time;
+  delete[] graph;
+  delete[] marks;
+  delete[] dp;
+  delete[] recursion_stack;
 }
